bb8cb58b26a33b5f6362f7236f0b4cdb6323edda
Ronald Blanco <pnald.blanco@gmail.com>
2017-06-01 14:19:45 +0000

    UpdateChart ready

--- app/controllers/publicController.client.js
+++ app/controllers/publicController.client.js
@@ -5,7 +5,10 @@ The client-side controller will be responsible for retrieving information from t
 and making it available within the view. 
 Additionally, it will specify what action should be taken when one of the two buttons are clicked.
 */
-
+//var Chart = require('./public/js/Chart.bundle.js');
+var mydata = [];
+var mybackcolors = [];
+var mylabels = [];
 var config = {
         type: 'pie',
         data: {
@@ -44,7 +47,7 @@ var config = {
 (function () {
    
    //FOR THE CHART////////////////////
-   //var Chart = require('./public/js/Chart.bundle.js');
+   
    //require('./public/js/utils.js');
    window.chartColors = {
 	red: 'rgb(255, 99, 132)',
@@ -60,6 +63,13 @@ window.randomScalingFactor = function() {
 };
 var randomScalingFactor = function() {
         return Math.round(Math.random() * 100);
+};
+
+window.randomScalingFactorNew = function(myVar) {
+	return (myVar > 0.5 ? 1.0 : -1.0) * Math.round(myVar * 100);
+};
+var randomScalingFactorNew = function(myVar) {
+        return Math.round(myVar * 100);
 };
 
 /////////////////////////////
@@ -82,12 +92,7 @@ var randomScalingFactor = function() {
          publicList.innerHTML = publicList.innerHTML +'<li><input type="radio" value="' +pollArray[a].user+'_'+pollArray[a].poll.name+ '" name="publicP" id="opts'+a+'">'+pollArray[a].poll.name+'</li>';
          
       }
-      //PAUSE
-      /*var seconds = 1;
-      var waitTill = new Date(new Date().getTime() + seconds * 1000);
-      while(waitTill > new Date()){}*/
-
-      //updateChart(data);
+      
    }
    
    function updatePublicOpt (data) {
@@ -98,58 +103,28 @@ var randomScalingFactor = function() {
          publicListOpt.innerHTML = publicListOpt.innerHTML +'<li><input type="radio" value="' +pollArray[a].user+'_'+pollArray[a].opt.name+'_'+pollArray[a].opt.nameopt+ '" name="publicPOpt" id="opts'+a+'">'+pollArray[a].opt.nameopt+'</li>';
          
       }
-            //updateChart(data);
+            
    }
    
    //Function CHART//////////////
    function updateChart (data){
+       var chartData = JSON.parse(data);
+       var origbackgroundColor = [
+                    window.chartColors.red,
+                    window.chartColors.orange,
+                    window.chartColors.yellow,
+                    window.chartColors.green,
+                    window.chartColors.blue,
+                ];
+       mydata = [];
+       mybackcolors = [];
+       mylabels = [];
+       for(var a = 0; a < chartData.length; a++){
+           mydata.push(chartData[a].opt.vote);
+           mybackcolors.push(origbackgroundColor[a]);
+           mylabels.push(chartData[a].opt.nameopt);
+       }
        
-       config.data.datasets.forEach(function(dataset) {
-            dataset.data = dataset.data.map(function() {
-                return randomScalingFactor();
-            });
-        });
-        window.myPie.update();
-      
-      /*window.onload = function() {
-        //var ctx = document.getElementById("chart-area").getContext("2d");
-        window.myPie = new Chart(chart, config);
-    };*/
-
-    /*document.getElementById('randomizeData').addEventListener('click', function() {
-        config.data.datasets.forEach(function(dataset) {
-            dataset.data = dataset.data.map(function() {
-                return randomScalingFactor();
-            });
-        });
-
-        window.myPie.update();
-    });*/
-
-    //var colorNames = Object.keys(window.chartColors);
-    /*document.getElementById('addDataset').addEventListener('click', function() {
-        var newDataset = {
-            backgroundColor: [],
-            data: [],
-            label: 'New dataset ' + config.data.datasets.length,
-        };
-
-        for (var index = 0; index < config.data.labels.length; ++index) {
-            newDataset.data.push(randomScalingFactor());
-
-            var colorName = colorNames[index % colorNames.length];;
-            var newColor = window.chartColors[colorName];
-            newDataset.backgroundColor.push(newColor);
-        }
-
-        config.data.datasets.push(newDataset);
-        window.myPie.update();
-    });*/
-
-    /*document.getElementById('removeDataset').addEventListener('click', function() {
-        config.data.datasets.splice(0, 1);
-        window.myPie.update();
-    });*/
    }
    //FUNCTION CHART END////////////////////////////////
 
@@ -159,15 +134,9 @@ var randomScalingFactor = function() {
    //LISTENERS
    voteB.addEventListener('click',function(){
       
-      var poll = document.querySelector('input[name = "publicP"]:checked').value;//{'user': user.github.id,'poll':poll}
-      ajaxFunctions.ready(ajaxFunctions.ajaxRequest('POST', apiUrl+'Chart/'+poll, updateChart));
-      /*config.data.datasets.forEach(function(dataset) {
-            dataset.data = dataset.data.map(function() {
-                return randomScalingFactor();
-            });
-        });
-
-        window.myPie.update();*/
+      //var poll = document.querySelector('input[name = "publicP"]:checked').value;//{'user': user.github.id,'poll':poll}
+      //ajaxFunctions.ready(ajaxFunctions.ajaxRequest('POST', apiUrl+'Chart/'+poll, updateChart));
+      
          
    },false);
    
@@ -181,6 +150,7 @@ var randomScalingFactor = function() {
       
       var poll = document.querySelector('input[name = "publicP"]:checked').value;//{'user': user.github.id,'poll':poll}
       ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl+"opt/"+poll, updatePublicOpt));
+      ajaxFunctions.ready(ajaxFunctions.ajaxRequest('POST', apiUrl+'Chart/'+poll, updateChart));
          
    },false);//GET OPTIONS OF A POLL
    
